f247ec15ccb7af2f4e28404ce6d2653ee8f16842
justicsc <82329123+justicsc@users.noreply.github.com>
2021-09-29 23:31:58 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,19 +3,36 @@
 
 using namespace std;
 
-struct Check{
-    int CheckNum;
-    string Checkmemo;
-    int CheckAmount;
+class Check{
+public:
+    Check():CheckNum(0), CheckMemo(),checkAmount(0){};
+    int getNum(){return CheckNum;}
+    string getMemo(){return CheckMemo;}
+    int getAmount(){return checkAmount;}
+    int setNum(int cNum){cNum=CheckNum;}  // set cNum to CheckNum
+    int setAmount(int amo){amo=checkAmount;} // set amo to checkAmount
+    string setMemo(string cMemo){cMemo=CheckMemo;}
+   // static int AllChecks(){return AllChecks;}
+   // static int number(){return number;}
 
-};
+private:
+        int CheckNum;
+        string CheckMemo;
+        int checkAmount;
+       // static int AllChecks;
+       // static int number;
+
+
+    };
+
+
 const int N =5;
 
 class CheckBook{
 public:
     CheckBook():balance(0),numOfChecks(0){};
     CheckBook(int b1):balance(b1), numOfChecks(0){};
-    Check check[N];
+    Check checks[N];
     int lastDeposit();
     int checkLimit();
     float getBalance(){return balance;}
@@ -23,61 +40,48 @@ public:
     void deposit(float amount);
     void displayChecks();
     bool writeCheck( float amount );
+    static int getallChecks(){return allChecks;}
+
 private:
+    static int allChecks;
     float balance;
     int numOfChecks;
 };
 
 
+
+
+
+
 int main() {
-    CheckBook c1;
-    c1.deposit(100);
-    c1.getBalance();
-    c1.displayChecks();
-    c1.lastDeposit();
-
-    std::cout << "Hello, World!" << std::endl;
+    CheckBook cb1(1000), cb2(500);
+    for(int i=0; i<10;i++)
+    {
+        cb1.writeCheck(150);
+        cb2.writeCheck(130);
+    }
+    cout<<"cb1 check details"<<endl;
+    cb1.displayChecks();
+    cout<<"cb2 check details"<<endl;
+    cb2.displayChecks();
+    cout<<cb1.getallChecks()<<endl;
+    cout<<cb2.getallChecks()<<endl;
     return 0;
-}
 
-int CheckBook::lastDeposit(){
-      return balance;
-                         //is supposed to show the last deposit in here
 
 };
+
+bool CheckBook::writeCheck( float amount ){
+     if(numOfChecks< N && allChecks < 10 && balance > 0 ){
+         return true;
+     }else{
+         return false;
+     }
+
+};
+
 void CheckBook::displayChecks(){
 
     cout<<numOfChecks<<endl; //displays numofchels
 
 };
-
-
-void CheckBook::deposit(float amount){
-      balance+=amount; //adds the amount
-
-};
-
-
-
-bool CheckBook::writeCheck( float amount ){
-    if(amount <= balance) {
-        balance++;
-        check[N];
-        numOfChecks++;
-        return true;
-    }else {
-        return false;
-
-    }
-
-
-
-};
-
-int CheckBook::checkLimit() {
-    if(N < 0)
-      cout<<"Array is empty  "<<endl;
-    else
-       return N;
-
-}
